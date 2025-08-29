ac6869a13f9d3a88667792ca612fe028f632d87f
HLaPides <hblapides@gmail.com>
2025-08-29 10:36:26 -0400

    Time: 1 ms (81.54%) | Memory: 40.7 MB (85.73%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int reverse(int x) 
+    {
+        int res = 0;
+        while (x != 0)
+        {
+                if (res>Integer.MAX_VALUE/10 || (res*10)+ x%10>Integer.MAX_VALUE/10 && x%10>7)
+                {
+                    return 0;
+                }
+                if (res<Integer.MIN_VALUE/10 || (res*10)+x%10<Integer.MIN_VALUE/10 && x%10<-8)
+                {
+                    return 0;
+                }
+            res = (res*10)+ x%10;
+            x /= 10;
+        }
+       return res; 
+    }
+}
